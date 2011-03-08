bf18680c9209b0c25701a3b8aa8822157c8d6779
Satoshi Fujiyoshi <hojiyoshi@gmail.com>
2011-03-09 01:09:35 +0900

    20110309 add address search from zipcode

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,6 @@
 function setToForm(){
     var curfrm = document.to_form;
-    if (curfrm.data_type_same.checked) {
+    if (curfrm.to_type_same.checked) {
         document.to_form.name2.disabled = "true";
         document.to_form.zip2.disabled = "true";
         document.to_form.addr2.disabled = "true";
